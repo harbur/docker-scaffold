a4268b08f47b7eea179eed944a3a729789280e4d
spiddy <d.kapanidis@gmail.com>
2015-10-23 16:58:17 +0200

    Check for obligatory variables can now accept a list of variables

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -2,10 +2,18 @@
 
 export APP_ID=${APP_ID:-000001}
 export OPTIONAL_VAR=${OPTIONAL_VAR:-}
-export OBLIGATORY_VAR=${OBLIGATORY_VAR}
+export VAR1=${VAR1}
+export VAR2=${VAR2}
 
+# Space separated list of Obligatory Variables
+OBLIGATORY_VARS="VAR1 VAR2"
 # Verify obligatory variables
-if [ -z "${OBLIGATORY_VAR}" ]; then echo "OBLIGATORY_VAR variable is not provided, exiting..."; exit -1; fi
+for VAR in $OBLIGATORY_VARS; do
+	if [ -z "${!VAR}" ]; then
+		echo "$VAR variable is not provided, exiting..."
+		exit -1
+	fi
+done
 
 echo "Generating app.properties..."
 envsubst < /templates/app.properties > /app.properties
